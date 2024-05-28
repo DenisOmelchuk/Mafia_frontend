f2c81a2a84d84bcdce6cc12c5e5d1b227ae549d8
Denys-Omelchuk <softwared757@gmail.com>
2024-05-28 12:52:49 +0300

    Add functions that allow to connect to API endpoint and to update user data

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { baseUrl } from "../shared";
 import img from "./Saul.png";
 import user_icon from "./user_icon.png";
 import cancel_icon_active from "./cancel_icon_active.png";
@@ -8,14 +10,75 @@ import friends_icon from "./friends_icon.png";
 import add_friends_icon from "./add_friends_icon.png";
 import a from "./a.jpg";
 
-export default function CreateGame() {
-  const [username, setUsername] = useState("Denny");
-  const [temproraryUsername, setTemproraryUsername] = useState("Denny");
+export default function Profile() {
+  const [username, setUsername] = useState("null");
+  const [temproraryUsername, setTemproraryUsername] = useState("null");
   const [image, setImage] = useState(a);
   const [temproraryImage, setTemproraryImage] = useState(null);
+  const [temproraryImageFile, setTemproraryImageFile] = useState(null);
+  const token = localStorage.getItem("access");
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+  const getUserData = async () => {
+    const url = baseUrl + "profile/";
+    try {
+      const response = await axios.get(url, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (response.status === 200) {
+        const { username, avatar } = response.data;
+        setUsername(username);
+        setTemproraryUsername(username);
+        setImage(baseUrl + avatar.substring(1));
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const updateUserData = async (event) => {
+    event.preventDefault();
+    const url = baseUrl + "profile/update/";
+    const formData = new FormData();
+  
+    if (temproraryImage !== null) {
+      formData.append("avatar", temproraryImageFile);
+    }
+  
+    if (temproraryUsername !== username) {
+      formData.append("username", temproraryUsername);
+    }
+  
+    try {
+      const response = await axios.put(url, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (response.status === 200) {
+        const { username, avatar } = response.data;
+        setUsername(username);
+        setTemproraryUsername(username);
+        setImage(baseUrl + avatar.substring(1));
+        setTemproraryImage(null);
+        setTemproraryImageFile(null);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
   const handleFileInput = (e) => {
     const file = e.target.files[0];
+    setTemproraryImageFile(file)
     if (file) {
       const imageURL = URL.createObjectURL(file);
       setTemproraryImage(imageURL);
@@ -25,6 +88,7 @@ export default function CreateGame() {
   const cancelChanges = () => {
     setTemproraryImage(null);
     setTemproraryUsername(username);
+    setTemproraryImageFile(null)
   };
 
   return (
@@ -33,6 +97,7 @@ export default function CreateGame() {
       <div className="create_page_container">
         <div className="create_game_img_form_container">
           <img src={img} alt="" className="heisengerg_img" />
+
           <div className="form_container">
             <div className="create_game_form_container_pararaph_container">
               <p>
@@ -52,13 +117,13 @@ export default function CreateGame() {
                     src={image}
                     alt=""
                     className="user_image_profile_info_original"
-                  ></img>
+                  />
                 ) : (
                   <img
                     src={temproraryImage}
                     alt=""
                     className="user_image_profile_info_new"
-                  ></img>
+                  />
                 )}
 
                 <input
@@ -69,12 +134,12 @@ export default function CreateGame() {
                   accept="image/png, image/jpeg"
                   onChange={handleFileInput}
                 />
-                <label for="image" className="profile_image_input">
+                <label htmlFor="image" className="profile_image_input">
                   Update Photo
                 </label>
               </div>
               <div className="username_info_container">
-                <img src={user_icon} alt="" className="user_icon"></img>
+                <img src={user_icon} alt="" className="user_icon" />
                 <input
                   type="text"
                   onChange={(e) => setTemproraryUsername(e.target.value)}
@@ -88,14 +153,14 @@ export default function CreateGame() {
                 />
               </div>
               <div className="profile_friends_container">
-                <img src={friends_icon} alt="" className="friends_icon"></img>
+                <img src={friends_icon} alt="" className="friends_icon" />
                 <button className="profile_friends_btn">See Friends</button>
                 <button className="profile_add_friend_btn">
                   <img
                     src={add_friends_icon}
                     alt=""
                     className="add_friends_icon"
-                  ></img>
+                  />
                 </button>
               </div>
               <div className="profile_btns_container">
@@ -113,17 +178,18 @@ export default function CreateGame() {
                       src={cancel_icon_deactive}
                       alt=""
                       className="cancel_btn_icon"
-                    ></img>
+                    />
                   ) : (
                     <img
                       src={cancel_icon_active}
                       alt=""
                       className="cancel_btn_icon"
                       onClick={cancelChanges}
-                    ></img>
+                    />
                   )}
                 </button>
                 <button
+                  onClick={updateUserData}
                   className="update_btn"
                   id={
                     temproraryUsername === username && temproraryImage === null
